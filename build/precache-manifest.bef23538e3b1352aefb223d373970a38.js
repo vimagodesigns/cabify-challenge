b44a60b895ede39c9ec84ec37d4387c3ee9b1d52
Victor Garcia <vimago81@gmail.com>
2019-09-17 15:18:56 +0200

    Adding build folder for the challenge

---
+++ build/precache-manifest.bef23538e3b1352aefb223d373970a38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f71e7a750b33958e85701e19c1a823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "064b6472c909a75b416a",
+    "url": "/static/css/main.3f2e7def.chunk.css"
+  },
+  {
+    "revision": "f60663b32802429ed15d",
+    "url": "/static/js/2.11186612.chunk.js"
+  },
+  {
+    "revision": "064b6472c909a75b416a",
+    "url": "/static/js/main.380383b5.chunk.js"
+  },
+  {
+    "revision": "51120b8e5382f0e4707e",
+    "url": "/static/js/runtime~main.8e147ef0.js"
+  }
+]);
